5e0158c510e096363a91fd58cc9db5193a7824fc
Erik Ashby <erik@ashbys.net>
2024-01-06 22:19:49 -0700

    Ledstrip action

---
+++ actions/ledstrip_fire.py
@@ -0,0 +1,11 @@
+# We Geek Together action
+
+# action: ledstrip_fire
+#
+# This action will simulate a fire with an RGB LED strip
+
+
+# This action requires neopixel.py to be in the same folder since it provides the interface to the RGB strip
+
+import time
+from neopixel import Neopixel
