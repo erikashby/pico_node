5f106ef3a522fa69d48e5cd2a3ab03594ac5f313
Erik Ashby <erik@ashbys.net>
2024-01-07 18:28:34 -0700

    update

--- actions/ledstrip_fire.py
+++ actions/ledstrip_fire.py
@@ -9,42 +9,48 @@
 # - neopixel.py to be in the root folder since it provides the interface to the RGB strip
 # - time for sleep actions
 
-import time,sys,random
-sys.path.append('/actions')
+import time,sys,random,uasyncio as asyncio
+#sys.path.append('/actions')
 from neopixel import Neopixel
 
-#define global configuration
-# Default number of led lights on strip
-led_strip_len = 60
-middle = int(led_strip_len/2)
 
-# Default IO pin used for led strip
-pico_gpio = 22
+
+
+
+
+
+
+async def main():  # Main function.
+    #define global configuration
+    # Default number of led lights on strip
+    led_strip_len = 60
+    middle = int(led_strip_len/2)
+
+    # Default IO pin used for led strip
+    pico_gpio = 22
     
-#initilise strip
-pixels = Neopixel(led_strip_len, 0, pico_gpio, "GRB")
+    #initilise strip
+    pixels = Neopixel(led_strip_len, 0, pico_gpio, "GRB")
     
-#Initiage main colors and brighness
-red=[255,0,0]
-yellow=[150,25,0]
-bright = 255
-medium = 100
-
-
-
-def show_line(bright_level,color1,color2,rotate):
+    #Initiage main colors and brighness
+    red=[255,0,0]
+    yellow=[150,25,0]
+    bright = 255
+    medium = 100
     
-    pixels.brightness(bright_level)
+    
+    async def show_line(bright_level,color1,color2,rotate):
+    
+        pixels.brightness(bright_level)
         
-    pixels.set_pixel_line_gradient(0, middle, color1,color2)
-    pixels.set_pixel_line_gradient(middle, led_strip_len-1, color2,color1)
+        pixels.set_pixel_line_gradient(0, middle, color1,color2)
+        pixels.set_pixel_line_gradient(middle, led_strip_len-1, color2,color1)
             
-    #rotate line
-    pixels.rotate_right(rotate)
-    pixels.show()
-
-
-def main():  # Main function.
+        #rotate line
+        pixels.rotate_right(rotate)
+        pixels.show()
+    
+    
     brightness_adjustment = 0
     rotate_adjustment = 0
 
@@ -65,20 +71,20 @@ def main():  # Main function.
         if rotate_adjustment > led_strip_len:
             rotate_adjustment=0
             
-        time.sleep(sleep_time)
+        asyncio.sleep(sleep_time)
         
         #burst flicker
         if int(random.randint(0,20)) == 1:
             show_line(bright-brightness_adjustment-25,yellow,red,rotate_adjustment)
-            time.sleep(0.02)
+            asyncio.sleep(0.02)
             show_line(bright-brightness_adjustment,yellow,red,rotate_adjustment)
-            time.sleep(.01)
+            asyncio.sleep(.01)
             show_line(bright-brightness_adjustment-25,yellow,red,rotate_adjustment)
-            time.sleep(.01)
+            asyncio.sleep(.01)
             show_line(bright-brightness_adjustment-70,yellow,red,rotate_adjustment)
-            time.sleep(sleep_time)
+            asyncio.sleep(sleep_time)
         
         #pause for some random time
 
-    
+print("starting")   
 main()
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 #main module
+import node as node, machine, uasyncio as asyncio
 #This module will run automatically when the pico starts up
 
 #Define the possible actions that are supported by the pico
 
+try:
+    node.connect()
+except KeyboardInterrupt:
+    machine.reset()
+
 #create a fire
 ledstrip_fire = __import__('/actions/ledstrip_fire.py', globals(), locals(), ['main'], 0)
 ledstrip_fire.main()
+
+#asyncio.create_task(ledstrip_fire.main())
+
+print("done")
+
+while True:
+    asyncio.sleep(1)
+    print("waiting")
+
+
